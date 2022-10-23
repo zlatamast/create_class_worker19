4a7cf5a5caad5bef24e6c2db72e0ff61280f884d
zlatamast <zlatamast@gmail.com>
2022-10-23 21:37:55 +0300

    init : calc

---
+++ index.js
@@ -0,0 +1,24 @@
+// Создайте класс Работник, который будет иметь следующие свойства: name (имя), surname (фамилия), rate (ставка за день работы), days (количество отработанных дней).
+// Также класс должен иметь метод getSalary(), который будет выводить зарплату работника. Зарплата - это произведение (умножение) ставки rate на количество отработанных дней days.
+
+class Worker  {
+    constructor (name, surname, rate, days) {
+        this.name = name;
+        this.surname = surname;
+        this.rate = rate;
+        this.days = days;
+    }
+
+    getSalary() {
+        let salary = this.rate * this.days;
+        return salary;
+    }
+}
+
+let worker = new Worker('Иван', 'Иванов', 10, 31);
+
+console.log(worker.name); //выведет 'Иван'
+console.log(worker.surname); //выведет 'Иванов'
+console.log(worker.rate); //выведет 10
+console.log(worker.days); //выведет 31
+console.log(worker.getSalary()); 
